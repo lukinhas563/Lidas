b84a8e5aa0cb95b76b60548997533ceb2a8a4072
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-07-25 14:18:53 -0300

    :tada: init: Create API Gateway and configure docker compose

--- Lidas.UserApi/Program.cs
+++ Lidas.UserApi/Program.cs
@@ -118,11 +118,8 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(corsPolicy);
 app.UseHttpsRedirection();
