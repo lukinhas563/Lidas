7fded9ce14feeda41cf928599527c9836ef2ad49
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-08-02 11:58:36 -0300

    :tada: init: Create likes service

---
+++ LikesApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LikesApi.Controllers
+{
+    [Route("api/like")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
